5465be1effd503811871db4468b22e9022f08d59
Kevin Labs <kevinlabs@outlook.com>
2017-04-06 12:46:08 -0600

    First upload

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp'),
+    sourcemaps = require('gulp-sourcemaps'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    CacheBuster = require('gulp-cachebust'),
+    print = require('gulp-print'),
+    babel = require('gulp-babel'),
+    uglify = require('gulp-uglify');
+
+
+var cachebust = new CacheBuster();
+
+//Build css from SAS
+
+gulp.task('build-css', function () {
+    return gulp.src('./styles/*')
+        .pipe(sourcemaps.init())
+        //Source maps is debugging thing. it's helping us to read.
+        .pipe(sass())
+        //Turn sass into valid CSS.
+        .pipe(cachebust.resources())
+
+        .pipe(concat('styles.css'))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build-js', function() {
+   return gulp.src('js/**/*.js')               
+      .pipe(sourcemaps.init())
+      .pipe(print())                        
+      .pipe(babel({ presets: ['es2015'] }))
+      .pipe(concat('bundle.js'))
+      //.pipe(uglify())
+      .pipe(sourcemaps.write('./')) 
+      .pipe(gulp.dest('./dist/js')); 
+});
+
+gulp.task('build', ['build-css', 'build-js'], function() {
+    return gulp.src('index.html')
+        .pipe(cachebust.references())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+    return gulp.watch(['./index.html','./partials/*.html', './styles/*.*css', './js/**/*.js'], ['build']);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+gulp.task('hello', function () {
+    console.log('Hello from my first gulp task.');
+});
+
+gulp.task('superMonkeyPower', function () {
+    console.log('Poowweeerrrr');
+});
